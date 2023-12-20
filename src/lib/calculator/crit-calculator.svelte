d1867fe8e90add50ad4fe2c66dd8f7de95023ca7
Carlos Felgueiras <catofel2000@gmail.com>
2023-12-20 00:29:04 +0000

    feature: flowbite test

---
+++ src/lib/calculator/crit-calculator.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { Input, Label, Helper } from 'flowbite-svelte';
+
+    let crit = 5;
+</script>
+
+<Input type="number" bind:value={crit} />
---
+++ src/lib/structure/genshin-nav-bar.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { page } from '$app/stores';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+
+    $: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar>
+	<NavBrand>
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Genshin Utilities</span>
+	</NavBrand>
+	<NavHamburger/>
+	<NavUl {activeUrl}>
+		<NavLi>
+			<a href="/">Home</a>
+		</NavLi>
+		<NavLi>
+			<a href="/">About</a>
+		</NavLi>
+		<NavLi>
+			<a href="/">Contact</a>
+		</NavLi>
+	</NavUl>
+</Navbar>
